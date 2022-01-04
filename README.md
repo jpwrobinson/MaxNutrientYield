a15fad165c8d201712d6322d7f8de3de2e86e735
James Robinson <jpwrobinson@gmail.com>
2022-01-04 12:48:22 +0000

    revised scripts and models

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This manuscript was produced using the following packages
 
 
 ```
-R version 4.0.2 (2020-06-22)
+R version 4.0.5 (2021-03-31)
 Platform: x86_64-apple-darwin17.0 (64-bit)
 
 Matrix products: default
@@ -19,24 +19,26 @@ attached base packages:
 [1] stats     graphics  grDevices utils     datasets  methods   base     
 
 other attached packages:
- [1] here_1.0.0       skimr_2.1.2      forcats_0.5.0    stringr_1.4.0   
- [5] dplyr_1.0.2      purrr_0.3.4      readr_1.4.0      tidyr_1.1.2     
- [9] tibble_3.0.5     ggplot2_3.3.2    tidyverse_1.3.0  mizer_2.0.4.9000
+ [1] here_0.1             skimr_2.1.1          forcats_0.5.0       
+ [4] stringr_1.4.0        dplyr_1.0.7          purrr_0.3.4         
+ [7] readr_2.0.2          tidyr_1.1.4          tibble_3.1.6        
+[10] ggplot2_3.3.5        tidyverse_1.3.0.9000 mizer_2.3.0.1       
 
 loaded via a namespace (and not attached):
- [1] xfun_0.19         tidyselect_1.1.0  repr_1.1.0        reshape2_1.4.4   
- [5] haven_2.3.1       colorspace_2.0-0  vctrs_0.3.6       generics_0.1.0   
- [9] htmltools_0.5.0   viridisLite_0.3.0 base64enc_0.1-3   utf8_1.1.4       
-[13] plotly_4.9.2.1    rlang_0.4.10      pillar_1.4.7      glue_1.4.2       
-[17] withr_2.4.0       DBI_1.1.0         dbplyr_2.0.0      modelr_0.1.8     
-[21] readxl_1.3.1      lifecycle_0.2.0   plyr_1.8.6        munsell_0.5.0    
-[25] gtable_0.3.0      cellranger_1.1.0  rvest_0.3.6       htmlwidgets_1.5.2
-[29] knitr_1.30.2      ps_1.4.0          fansi_0.4.2       broom_0.7.2      
-[33] Rcpp_1.0.5        scales_1.1.1      backports_1.2.0   jsonlite_1.7.2   
-[37] fs_1.5.0          hms_1.0.0         digest_0.6.27     stringi_1.5.3    
-[41] rprojroot_2.0.2   grid_4.0.2        cli_2.2.0         tools_4.0.2      
-[45] magrittr_2.0.1    lazyeval_0.2.2    pacman_0.5.1      crayon_1.3.4     
-[49] pkgconfig_2.0.3   ellipsis_0.3.1    data.table_1.13.2 xml2_1.3.2       
-[53] reprex_0.3.0      lubridate_1.7.9.2 assertthat_0.2.1  httr_1.4.2       
-[57] rstudioapi_0.13   R6_2.5.0          compiler_4.0.2   
+ [1] Rcpp_1.0.7        lubridate_1.8.0   rprojroot_2.0.2   assertthat_0.2.1 
+ [5] digest_0.6.28     utf8_1.2.2        R6_2.5.1          cellranger_1.1.0 
+ [9] plyr_1.8.6        repr_1.1.0        backports_1.3.0   reprex_0.3.0     
+[13] httr_1.4.2        pillar_1.6.4      rlang_0.4.12      lazyeval_0.2.2   
+[17] readxl_1.3.1      rstudioapi_0.13   data.table_1.14.2 htmlwidgets_1.5.4
+[21] munsell_0.5.0     broom_0.7.10      compiler_4.0.5    modelr_0.1.8     
+[25] xfun_0.27         base64enc_0.1-3   pkgconfig_2.0.3   htmltools_0.5.2  
+[29] tidyselect_1.1.1  fansi_0.5.0       viridisLite_0.4.0 crayon_1.4.2     
+[33] tzdb_0.1.2        dbplyr_2.1.1      withr_2.4.2       grid_4.0.5       
+[37] jsonlite_1.7.2    gtable_0.3.0      lifecycle_1.0.1   DBI_1.1.1        
+[41] pacman_0.5.1      magrittr_2.0.1    scales_1.1.1      cli_3.1.0        
+[45] stringi_1.7.5     reshape2_1.4.4    fs_1.5.0          xml2_1.3.2       
+[49] ellipsis_0.3.2    generics_0.1.1    vctrs_0.3.8       tools_4.0.5      
+[53] glue_1.5.0        hms_1.1.1         fastmap_1.1.0     colorspace_2.0-2 
+[57] rvest_1.0.1       plotly_4.10.0     knitr_1.28        haven_2.3.0      
+
 ```
--- simulate_NorthSea_nutrient_curve.R
+++ simulate_NorthSea_nutrient_curve.R
@@ -20,24 +20,12 @@ load(file = 'NorthSea_calibrated.rds')
 ##clean some params
 params@gear_params$initial_effort<-NULL
 
-### set starting effort position = time-averaged 2000-10
-calib_years<-c(2000:2010)
-start_effort<-t(as.matrix(colMeans(subset(effort, rownames(effort) %in% calib_years))))
+
+### set starting effort position = simulated FMSY
+start_effort<-t(as.matrix(fmsy$fmsy))
+colnames(start_effort)<-fmsy$species
 rownames(start_effort)<-'2010'
 
-
-## set up range of fishing effort
-f=seq(0, 3, 0.05)
-
-### set starting effort position = time-averaged 2000-10
-calib_years<-c(2000:2010)
-start_effort<-t(as.matrix(colMeans(subset(effort, rownames(effort) %in% calib_years))))
-rownames(start_effort)<-'2010'
-
-## get F baseline as average last 20 years
-f_base<-start_effort
-
-## set up unfished biomass for stock collapse definition
 unfished <- project(params, effort = 0, dt = 0.25, t_save = 1, t_max=100)
 unfish.biom<-getBiomass(unfished, min_w = 10, max_w = 100e3) %>% data.frame() %>%
   mutate(year = rownames(getBiomass(unfished))) %>%
@@ -45,6 +33,15 @@ unfish.biom<-getBiomass(unfished, min_w = 10, max_w = 100e3) %>% data.frame() %>
   group_by(year, species) %>%
   summarise(unfished_biomass = biomass) %>% 
   filter(year == 99)
+
+unfish_ref<-sum(unfish.biom$unfished_biomass)
+
+## and shift F by a multiplier (not set at fixed value for all species)
+f=seq(0, 20, by =0.05)
+
+## get F baseline as simulated FMSY£
+f_base<-start_effort
+
 
 ## empty vectors to be filled in simulations
 yield_species<-numeric() 
@@ -54,80 +51,91 @@ mean_weight<-numeric()
 collapsed<-numeric() 
 
 ## loop over each f value, simulate to 2050, save 2050 results
+i=0
+ repeat{
+    i = i + 1
+    
+    f.run <- c(Sprat = f_base[11]*f[i], Sandeel = f_base[9]*f[i], N.pout = f_base[6]*f[i], 
+              Herring = f_base[5]*f[i], Dab = f_base[2]*f[i],
+              Whiting = f_base[12]*f[i], Sole = f_base[10]*f[i], Gurnard = f_base[3]*f[i], 
+              Plaice = f_base[7]*f[i], Haddock = f_base[4]*f[i],
+              Cod = f_base[1]*f[i], Saithe = f_base[8]*f[i])
 
-for(i in 1:length(f)){
-    
-    # update the fishing effort for each species
-    f.run <- c(Sprat = f[i], Sandeel = f[i], N.pout = f[i], Herring = f[i], Dab = f[i],
-              Whiting = f[i], Sole = f[i], Gurnard = f[i], Plaice = f[i], Haddock = f[i],
-              Cod = f[i], Saithe = f[i])
-    
-    ## setup 89 years of fishing at the fixed f level (to equilbrium at 2050)
-    scenario2 <- t(array(f.run, dim=c(12,90), dimnames=list(NULL,year = 2011:2050)))
-    
-    # add historic effort up to 2010, 
-    # then linearly shift towards f.run levels from 2010-2015
+    scenario2 <- t(array(f.run, dim=c(12,90), dimnames=list(NULL,year = 2011:2100)))
+
+    # start from time-averaged 2000-2010, then linearly shift towards f.run levels over 5 years
     scenario2 <- rbind(start_effort, scenario2)
     for (sp in dimnames(scenario2)[[2]]){
-      scenario2[as.character(2011:2020),sp] <- scenario2["2010",sp] +
-        (((scenario2["2020",sp] - scenario2["2010",sp]) / 10) * 1:10)
+      scenario2[as.character(2011:2015),sp] <- scenario2["2010",sp] +
+        (((scenario2["2015",sp] - scenario2["2010",sp]) / 5) * 1:5)
     }
-    
-    ## run simulation
+
     sim2 <- project(params, effort = scenario2, dt = 0.25, t_save = 1)
     
-    ## save total biomass
+    ## ssb
+    gb<-plotBiomass(sim2)
+    
     t<-getBiomass(sim2, min_w = 10, max_w = 100e3) %>% data.frame() %>%
         mutate(year = rownames(getBiomass(sim2))) %>%
         pivot_longer(-year, names_to = 'species', values_to = 'biomass') %>%
         group_by(year) %>%
         summarise(biomass = sum(biomass)) %>%
-        filter(year == 2050) %>% 
+        filter(year == 2100) %>% 
         mutate(f = f[i])
     
-    ## save species biomass
     bs<-getBiomass(sim2, min_w = 10, max_w = 100e3) %>% data.frame() %>%
       mutate(year = rownames(getBiomass(sim2))) %>%
       pivot_longer(-year, names_to = 'species', values_to = 'biomass') %>%
       group_by(year, species) %>%
       summarise(biomass = sum(biomass)) %>%
-      filter(year == 2050) %>% 
+      filter(year == 2100) %>% 
       mutate(f = f[i])
     
-    ## save collapsed species
+    ## collapsed species
     collapse<-getBiomass(sim2, min_w = 10, max_w = 100e3) %>% data.frame() %>%
       mutate(year = rownames(getBiomass(sim2))) %>%
       pivot_longer(-year, names_to = 'species', values_to = 'biomass') %>%
       group_by(year, species) %>%
       summarise(min_biomass = min(biomass)) %>%
-      filter(year %in% c(2050))
+      filter(year %in% c(2100))
     
-    ## define stock collapse as <10% of unfished biomass
     collapse$unfished_biomass<-unfish.biom$unfished_biomass[match(collapse$species, unfish.biom$species)]
     collapse$prop<-with(collapse, min_biomass / unfished_biomass)*100
     collapse$collapsed<-ifelse(collapse$prop < 10, 1, 0)
     collapse$f <- f[i]
     
-    cl<-data.frame(n.collapse = sum(collapse$collapsed), f = f[i])
+    collapsed<-data.frame(n.collapse = sum(collapse$collapsed), f = f[i])
     
-    ## save mean weight
+    ## mean weight
+    gmw<-getMeanWeight(sim2, min_w = 10, max_w = 100e3) %>% data.frame() %>% 
+      ggplot() + geom_line(aes(rownames(scenario2)[1]:last(rownames(scenario2)), .)) +
+      labs(x = '', y = 'Mean weight, kg')
+    
     w<-getMeanWeight(sim2, min_w = 10, max_w = 100e3) %>% last()
-    w<-data.frame(mean_weight = w, year=2050, f = f[i])
+    w<-data.frame(mean_weight = w, year=2100, f = f[i])
     
-    ## save total catch (yield)
+    ## yield
     y<-data.frame(getYield(sim2))
     y$year<-as.numeric(rownames(y))
-    y<-y %>% 
-      pivot_longer(-year, names_to = 'species', values_to = 'yield') %>% 
-      filter(year == 2050) %>% mutate(f = f[i])
+    y<-y %>% pivot_longer(-year, names_to = 'species', values_to = 'yield')
     
-    ## bind result and loop to next simulated F value
-    yield_species<-rbind(yield_species, y)
-    tot_biomass<-rbind(tot_biomass, t)
-    biomass_species<-rbind(biomass_species, data.frame(bs))
-    mean_weight<-rbind(mean_weight, w)
-    collapsed<-rbind(collapsed, cl)
+    gy<-ggplot(y, aes(year, yield, col=species)) + geom_line() +
+      theme(legend.position = c(0.2, 0.6)) +
+      labs(x = '', y = 'Yield, tonnes', title = paste0('Fishing mortality = ', f[i]))
+    
+    
+    print(cowplot::plot_grid(gy, gb, gmw, nrow=1))
+    
+    
+    y<-y %>% filter(year == 2100) %>% mutate(f = f[i])
+    
+    yf<-rbind(yf, y)
+    tb<-rbind(tb, t)
+    bsp<-rbind(bsp, data.frame(bs))
+    mw<-rbind(mw, w)
+    cl<-rbind(cl, collapsed)
   
+  if( t$biomass / unfish_ref < 0.1 ){break}
   }
 
 ## match in nutrient concentrations
@@ -180,6 +188,17 @@ curves<-rbind(y.nut.scale %>% mutate(group = nutrient, value = nut.yield, scaled
 curves$group<-factor(curves$group, 
                       levels=c('calcium.mg', 'iron.mg', 'selenium.mug', 'zinc.mg', 'vitamin_a.mug', 'vitamin_d.mug',
                                'omega3.g','Nutrient density', 'Total yield', 'Fishable biomass', 'Mean weight', 'Collapsed species'))
+
+## fishing mortality metric
+f_mort<-data.frame(f = y_nut$f[y_nut$nutrient=='calcium.mg'], 
+            yield = y_nut$yield[y_nut$nutrient=='calcium.mg']) %>% 
+    left_join(tot_biomass, by = 'f')
+f_mort$f_mort<-f_mort$yield / f_mort$biom
+
+curves$f_mort<-f_mort$f_mort[match(curves$f, f_mort$f)]
+yield_species$f_mort<-f_mort$f_mort[match(yield_species$f, f_mort$f)]
+nut_yield_species$f_mort<-f_mort$f_mort[match(nut_yield_species$f, f_mort$f)]
+
 
 ## save output
 save(
